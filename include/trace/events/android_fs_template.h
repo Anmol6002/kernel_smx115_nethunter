25b861f4912a64a056e93700d638c91aed00161e
Anmol Singh <162159622+Anmol6002@users.noreply.github.com>
2025-07-18 09:43:54 +0530

    Update android_fs_template.h

--- include/trace/events/android_fs_template.h
+++ include/trace/events/android_fs_template.h
@@ -165,10 +165,10 @@ DECLARE_EVENT_CLASS(android_fs_separation_template,
 			__entry->append_ratio = arr[10];
 		}
 	),
-	TP_printk("entry_name %s, offset %llu, bytes %lld, cmdline %s, pid %lld, "
+	TP_printk("entry_name %s, offset %llu, bytes %d, cmdline %s, pid %d, "
           	"i_size %llu, ino %lu, time %llu, mtime %llu, current_time %llu, "
 	        "mtime_cnt %d, is_cache %d, is_fuse %d, write_chunk %lld, "
-	        "overwrite_cnt %d, append_cnt %d, overwrite_ratio %d, append_ratio %d, "
+	        "overwrite_cnt %d, append_cnt %d, overwrite_ratio %lld, append_ratio %lld, "
 	        "calculate_cold %lld",
 	        __get_str(pathbuf), __entry->offset, __entry->bytes, __get_str(cmdline),
 	        __entry->pid, __entry->i_size, (unsigned long) __entry->ino,
